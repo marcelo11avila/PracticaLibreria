c00337f787f36d415c08fedec961e3afb1921f40
Marcelo11 <Marcelo11@Cronos>
2018-01-10 19:36:58 +0100

    Cronos

---
+++ PracticaLibreria/src/Principal.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import libreria.Lista;
+import libreria.Diccionario;
+/**
+ *
+ * @author Marcelo11
+ */
+
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
